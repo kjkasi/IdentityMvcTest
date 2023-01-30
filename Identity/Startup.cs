64f27430dcb33929fd796ef46fb3065b395393ec
root <localhost@localdomain>
2023-01-30 15:39:02 +0300

    Настройка и Запуск Identity Server

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -1,5 +1,9 @@
+using Identity.Data;
+using Identity.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,11 +26,32 @@ namespace Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(opt =>
+                opt.UseInMemoryDatabase("InMem"));
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            var builder = services.AddIdentityServer(opt =>
+            {
+                opt.Events.RaiseErrorEvents = true;
+                opt.Events.RaiseInformationEvents = true;
+                opt.Events.RaiseFailureEvents = true;
+                opt.Events.RaiseSuccessEvents = true;
+
+                opt.EmitStaticAudienceClaim = true;
+            }).AddInMemoryIdentityResources(SD.IdentityResources)
+            .AddInMemoryApiScopes(SD.ApiScopes)
+            .AddInMemoryClients(SD.Clients)
+            .AddAspNetIdentity<AppUser>();
+
+            builder.AddDeveloperSigningCredential();
+
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDbContext context)
         {
             if (env.IsDevelopment())
             {
@@ -36,9 +61,14 @@ namespace Identity
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            context.Database.EnsureCreated();
+
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseIdentityServer();
 
             app.UseAuthorization();
 
